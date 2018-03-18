96917fa58f3140abc511e036922e6cc839737d61
asavoaeiandreea <andreea.asavoaei98@e-uvt.ro>
2018-03-18 19:24:52 +0200

    Definire funcţii suplimentare.

--- Proiect ASD2/main.c
+++ Proiect ASD2/main.c
@@ -8,40 +8,68 @@ struct Produs{
   float pretProdus;
 };
 
-/*struct Cos{
-  char Produs[30];
+typedef struct nod{
+  struct Produs p[];
   int cantitateProdus;
+  struct nod *urm;
+};Tnod
 
-};*/
-
+typedef struct lista{
+  Tnod *primul, *ultimul;
+}Tlista;
 
 char nume[15], prenume[15], adresa[30];
 long numarTelefon;
 int plata, livrare;
+char ch;
 
-FILE *fproduse;
+FILE *fproduse, *bproduse;
 
-void citireTabelProduse(int *n, struct Produs p[])
+void citireTabelProduseFemei(int *n, struct Produs f[])
 { int i;
-  fproduse=fopen("produse.txt","r");
+  fproduse=fopen("produseFemei.txt","r");
   if(fproduse==NULL)
-    { printf("Fisierul nu a putut fi deschis\n");
+    { printf("Fisierul cu produse pentru femei nu a putut fi deschis\n");
       exit(1);
     }
   fscanf(fproduse,"%d",n);
   for(i=0;i<*n;i++)
-    { fscanf(fproduse,"%d",&p[i].codProdus);
-      fscanf(fproduse,"%s",p[i].numeProdus);
-      fscanf(fproduse,"%f",&p[i].pretProdus);
+    { fscanf(fproduse,"%d",&f[i].codProdus);
+      fscanf(fproduse,"%s",f[i].numeProdus);
+      fscanf(fproduse,"%f",&f[i].pretProdus);
     }
 }
 
-void afisareTabelProduse(int *n, struct Produs p[])
+void citireTabelProduseBarbati(int *n, struct Produs b[])
+{ int i;
+  bproduse=fopen("produseBarbati.txt","r");
+  if(bproduse==NULL)
+    { printf("Fisierul cu produse pentru barbati nu a putut fi deschis\n");
+      exit(1);
+    }
+  fscanf(bproduse,"%d",n);
+  for(i=0;i<*n;i++)
+    { fscanf(bproduse,"%d",&b[i].codProdus);
+      fscanf(bproduse,"%s",b[i].numeProdus);
+      fscanf(bproduse,"%f",&b[i].pretProdus);
+    }
+}
+
+void afisareTabelProduseFemei(int *n, struct Produs f[])
 { int i;
   printf("COD PRODUS\tNUME PRODUS\tPRET PRODUS\n);
   for(i=0;i<*n;i++)
   {
-    printf("%4d\t%30s\t%.2f\n",p[i].codProdus,p[i].numeProdus,p[i].pretProdus);
+    printf("%3d\t%30s\t%.2f\n",f[i].codProdus,f[i].numeProdus,f[i].pretProdus);
+  }
+}
+
+void afisareTabelProduseBarbati(int *n, struct Produs b[])
+{ int i;
+  printf("COD PRODUS\tNUME PRODUS\tPRET PRODUS\n);
+  for(i=0;i<*n;i++)
+  {
+    printf("%3d\t%30s\t%.2f\n",b[i].codProdus,b[i].numeProdus,b[i].pretProdus);
   }
 }
 
@@ -69,7 +97,7 @@ void plataSiLivrare()
 
 void sumarComanda()
 {
-  printf("SUMAR COMANDA\n;
+  printf("SUMAR COMANDA\n);
   printf("DATE PERSONALE CLIENT:\n NUME%15s\n PRENUME%15s\n ADRESA LIVRARE%30s\n NUMAR DE TELEFON%ld\n",nume, prenume, adresa, numarTelefon);
   printf("PRODUSE ADAUGATE IN COS:\n);
   afisareLista();
@@ -94,8 +122,87 @@ void sumarComanda()
 
 
 void creareLista()
-void adaugareProduseInLista()
-void listaVida()
+
+int verificaProdus(int *n,int cod, struct Produs p[])
+{
+    int i;
+    for(i=0;i<*n;i++)
+    {
+        if(cod==f[i].codProdus||cod==b[i].codProdus)
+            return 1;
+        else
+            return 0;
+    }
+}
+void adaugareProduseInLista(Tlista *lista, int *n, int cod, struct Produs f[], struct Produs b[])
+{
+    Tnod *p=cautaProdusInCos;
+    if(p==NULL)
+    {
+        Tnod *nodNou;
+        nodNou=(Tnod *)malloc(sizeof(Tnod));
+        if(nodNou==NULL)
+        {
+            printf("EROARE!\n");
+            exit(1);
+        }
+        if(ch=='f')
+            if(verificaProdus(&n,cod,f))
+                if(produsExistent(&lista,cod)
+                   {
+                       printf("Produsul exista deja in cos! Doriti sa mai adaugati o bucata? [d/n]\n");
+                       char optiune;
+                       scanf("%c",&optiune);
+                       if(optiune=='d')
+                            marireCantitate(&lista,cod);
+                       else
+                            {   printf("Doriti sa eliminati produsul din cos? [d/n]\n");
+                                scanf("%c",&optiune);
+                                if(optiune=='d')
+                                    micsorareCantitate(&lista,cod);
+                            }
+                   }
+                else{   nodNou->p->codProdus=f.codProdus;
+                    strcpy(nodNou->p->numeProdus,f.numeProdus);
+                    nodNou->p->pretProdus=f.pretProdus;
+                    nodNou->cantitateProdus=1;
+                    nodNou->urm=NULL;
+                    if(lista->primul==NULL)
+                        lista->primul=lista->ultimul=nodNou;
+                    else
+                    {
+                        lista->ultimul->urm=nodNou;
+                        lista->ultimul=nodNou;
+                    }
+                }
+
+        else if(ch=='b')
+            if(verificaProdus(&n,cod,b))
+            {   nodNou->p->codProdus=b.codProdus;
+                strcpy(nodNou->p->numeProdus,b.numeProdus);
+                nodNou->p->pretProdus=b.pretProdus;
+                nodNou->cantitateProdus=1;
+                nodNou->urm=NULL;
+                if(lista->primul==NULL)
+                    lista->primul=lista->ultimul=nodNou;
+                else
+                {
+                    lista->ultimul->urm=nodNou;
+                    lista->ultimul=nodNou;
+                }
+            }
+
+
+    }
+}
+
+void vidare(Tlista *lista)
+{
+    lista->primul=lista->ultimul=NULL;
+    printf("Cosul de cumparaturi este gol\n");
+}
+
+void listaPlina()
 void stergeProdusDinLista()
 void adaugareProdusLaFavorite()
 void marireCantitate()
@@ -105,5 +212,45 @@ float totalPlata()
 
 int main()
 {
-
+    Tlist cosCumparaturi;
+    int *n;
+    struct Produs f[15];
+    struct Produs b[15];
+    //vidare(&cosCumparaturi);
+    citireTabelProduseFemei(&n,f);
+    citireTabelProduseBarbati(&n,b);
+    unsigned short optiune;
+    printf("\t\t MENIU\n");
+    printf("\t 1. Afiseaza lista cu produse\n");
+    printf("\t 2. Vidare - se initializeaza cosul de cumparaturi\n");
+    printf("\t 3. Adauga produs in cos\n");
+    printf("\t 4. Adauga produs la favorite\n");
+    printf("\t 5. Sterge produs din cos\n");
+    printf("\t 6. Goleste cosul de cumparaturi\n");
+    printf("\t 7. Afiseaza sumarul comenzii\n");
+    printf("\t 8. Procesare comanda\n");
+    printf("\t 9. Inchide aplicatia\n");
+    printf("Introduceti o cifra corespunzatoare actiunii dorite:\n");
+    scanf("%hi",&optiune);
+    do{
+        while(optiune<1 || optiune>9)
+        {
+            printf("EROARE: Valoarea introdusa trebuie sa fie din intervalul [1,9]!\n");
+            printf("Introduceti alta cifra corespunzatoare actiunii dorite:\n");
+            scanf("%hi",&optiune);
+        }
+        switch(optiune)
+        {
+            case 1: printf("Ce lista doriti sa afisati? f-femei, b-barbati\n");
+                    scanf("%c",ch);
+                    if(tolower(ch)=='f')
+                        afisareTabelProduseFemei(&n,f);
+                    else
+                        afisareTabelProduseBarbati(&n,b);
+                    break;
+            case 2: vidare(&cosCumparaturi);
+                    break;
+            case 3: adaugareProduseInLista()
+        }
+    }
 }
